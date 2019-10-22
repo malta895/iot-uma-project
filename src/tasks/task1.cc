4d5e3897ea22f2d1c5a36e49d42d517c11a21a49
Luca Maltagliati <malta95@hotmail.it>
2019-10-22 20:03:39 +0200

    Added Tasks 1-4

---
+++ src/tasks/task1.cc
@@ -0,0 +1,16 @@
+#define LED_NUMBER 13
+
+void setup() {
+  // initialize digital pin ledPin as an output.
+  int ledPin = LED_NUMBER;
+  pinMode(ledPin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  int ledPin = LED_NUMBER;
+  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
